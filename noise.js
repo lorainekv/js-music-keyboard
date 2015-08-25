5156c64f0695e0f74c4cd2271d5efc9a2357ddec
Loraine <loraine@lorainekv.com>
2015-08-25 16:45:30 -0700

    Got one key working. Trying to create functions to refactor.

--- noise.js
+++ noise.js
@@ -1,11 +1,21 @@
 $(document).ready( function() {
-
 // attach click handler to the right button
 // figure out which note to play
 // make the audio tag play
-var audio_tag = document.getElementById('aAudio');
-audio_tag.current_time = 0 // rewind the audio file
-audio_tag.play(); // this plays it exactly once
+
+  $(".a").click( function (){
+    var audio_tag = document.getElementById('aAudio');
+    audio_tag.current_time = 0 // rewind the audio file
+    audio_tag.play(); // this plays it exactly once
+  });
+
+
+
+
+
+  // var audio_tag = document.getElementById('aAudio');
+  // audio_tag.current_time = 0 // rewind the audio file
+  // audio_tag.play(); // this plays it exactly once
 
 
 
