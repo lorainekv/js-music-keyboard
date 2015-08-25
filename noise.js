7960311b990de245f7d9b3145dfbdb54e7b753d7
Loraine <loraine@lorainekv.com>
2015-08-25 16:20:15 -0700

    Live code setup: playing the audio

--- noise.js
+++ noise.js
@@ -1,3 +1,12 @@
 $(document).ready( function() {
-  // your code here
+
+// attach click handler to the right button
+// figure out which note to play
+// make the audio tag play
+var audio_tag = document.getElementById('aAudio');
+audio_tag.current_time = 0 // rewind the audio file
+audio_tag.play(); // this plays it exactly once
+
+
+
 });
